59f5f214e4b54d1212619ff4ade79c33dbb9d55b
dw-glitch <soudebararaka@gmail.com>
2026-09-22 16:30:10 -0300

    feat(cover): add Vite cover build

--- vite.config.ts
+++ vite.config.ts
@@ -39,6 +39,12 @@ const BUILDS = {
     bundleName: "GrconSigemPwDashboardBundle",
     emptyOutDir: false,
   },
+  cover: {
+    entry: "src/react/cover/index.tsx",
+    fileName: "cover-app.js",
+    bundleName: "GrconCoverBundle",
+    emptyOutDir: false,
+  },
 } as const;
 
 export default defineConfig(({ mode }) => {
